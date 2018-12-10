d004c3a311935b8860468fa0abe5fdd0c0570ac0
tkoch <tkoch@TKOCHHP7>
2018-12-10 13:38:11 -0500

    Add project files.

---
+++ tommvc3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tommvc3.Startup))]
+namespace tommvc3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
